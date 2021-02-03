7a8fc0f03c68d2c5f7352c684db0c9eb187f17dc
TheGreatStorm <thegreatstormx@gmail.com>
2021-02-03 11:21:12 -0600

    Removing random py code.

--- test-api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-username = input("Input your github username: ")
-password = input("Input your github pass/token: ")
-
-
-r = requests.get('https://api.github.com/repos/thegreatstorm/raging-automation/contents/rustserver/test.yml?ref=main', auth=(username, password))
-print(r.status_code)
